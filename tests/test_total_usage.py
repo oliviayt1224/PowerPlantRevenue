106b6ee182428ba01481ba4e09582b327d303c3e
yutingli <oliviaytli@163.com>
2025-02-23 11:46:47 +0000

    Added rounding.

--- tests/test_total_usage.py
+++ tests/test_total_usage.py
@@ -1,15 +1,8 @@
-import sys
-import os
+from src import functions
 
-# Add the 'src' directory to sys.path
-src_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../src'))
-sys.path.append(src_path)
-
-# Now you can import from the src module
-import functions
 
 def test_total_usage_check():
     sim = functions.Simulation()
     df, total_power_usage = sim.calculate_revenue()
-    assert sum(df["Power Usage"]) <= total_power_usage
+    assert round(sum(df["Power Usage"]),6) <= total_power_usage
 
