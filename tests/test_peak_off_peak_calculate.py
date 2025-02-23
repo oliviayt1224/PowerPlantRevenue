9a11d2717e398a5cca651d92b338f5f5cb3b92c6
yutingli <oliviaytli@163.com>
2025-02-23 11:50:09 +0000

    Added 2 more tests in it.

--- tests/test_peak_off_peak_calculate.py
+++ tests/test_peak_off_peak_calculate.py
@@ -2,8 +2,9 @@ from src import functions
 import pytest
 
 @pytest.mark.parametrize("month, peak_hours, off_peak_hours, days_in_month", [
-    ("2025/2", 280, 392, 28),
-
+    ("2025/02", 280, 392, 28),
+    ("2024/02", 294, 402, 29),
+    ("2025/03", 294, 450, 31)
 ])
 
 def test_peak_off_peak_calculation(month, peak_hours, off_peak_hours, days_in_month):
